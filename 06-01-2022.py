72c3d0778b78c7290950f7942f9266c9e4a272ae
teja802 <95519041+teja802@users.noreply.github.com>
2022-01-08 08:35:02 +0530

    Add files via upload

---
+++ 06-01-2022.py
@@ -0,0 +1,10 @@
+class A:
+    def __init__(self):
+        print('Initializing: class A')
+        def sub_method(self,b):
+            print('Printing from class A:',b)
+            class B(A)
+                def __init__(self):
+                    print('Initializing: class B')
+                    super(),__init__()
+                    def sub_method(self,b):
